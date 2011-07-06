7550eb36cffffb5ceb9c1edf51501e064e11c33e
Shaun Sephton <shaunsephton@gmail.com>
2011-07-06 13:00:20 +0200

    resolve to class. allow for exclude

--- object_tools/templatetags/object_tools_inclusion_tags.py
+++ object_tools/templatetags/object_tools_inclusion_tags.py
@@ -1,3 +1,5 @@
+import inspect
+
 from django import template
 
 from object_tools import tools
@@ -5,13 +7,20 @@ from object_tools import tools
 register = template.Library()
 
 @register.inclusion_tag('object_tools/inclusion_tags/object_tools.html')
-def object_tools(cl):
-    model = cl.model
-    
-    if tools._registry.has_key(model):
-        object_tool_classes = tools._registry[model]
+def object_tools(model, exclude=None):
+    if inspect.isclass(model):
+        model_class = model
     else:
-        object_tools = []
+        model_class = model.__class__
+
+    if tools._registry.has_key(model_class):
+        object_tool_classes = tools._registry[model_class]
+    else:
+        object_tool_classes = []
 
     object_tools = [object_tool for object_tool in object_tool_classes]
+
+    if exclude:
+        object_tools.remove(exclude)
+    
     return {'object_tools': object_tools}
