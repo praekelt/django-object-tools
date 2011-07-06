285df1c088a529e3949ba3b5e81c61ca409661f8
Shaun Sephton <shaunsephton@gmail.com>
2011-07-06 12:59:26 +0200

    simply wrapped shell view

--- object_tools/options.py
+++ object_tools/options.py
@@ -4,8 +4,24 @@ class ObjectTool():
     def __init__(self, model):
         self.model = model
 
-    def view(self, request, extra_context=None):
-        raise NotImplementedError('view method not implemented for %s' % self)
+    def _view(self, request, extra_context=None):
+        try:
+            self.view
+        except AttributeError:
+            raise NotImplementedError('view method not implemented for %s' % self)
+
+        opts = self.model._meta
+        app_label = opts.app_label
+        object_name = opts.object_name.lower()
+        extra_context = {
+            'user': request.user,
+            'title': 'Export %s' % opts.verbose_name_plural.lower(),
+            'tool': self,
+            'opts': opts,
+            'app_label': app_label,
+            'changelist_url': reverse('admin:%s_%s_changelist' % (app_label, object_name))
+        }
+        return self.view(request, extra_context)
     
     def get_urls(self):
         from django.conf.urls.defaults import patterns, url
@@ -14,7 +30,7 @@ class ObjectTool():
 
         urlpatterns = patterns('',
             url(r'^%s/$' % self.name,
-                self.view,
+                self._view,
                 name='%s_%s_%s' % info),
         )
         return urlpatterns
