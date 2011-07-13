8f2825751afbe4dc735858f18a160c5aced08a44
Shaun Sephton <shaunsephton@gmail.com>
2011-07-13 16:52:49 +0200

    general reordering of models

--- object_tools/options.py
+++ object_tools/options.py
@@ -7,9 +7,57 @@ from django.views.decorators.csrf import csrf_protect
 
 csrf_protect_m = method_decorator(csrf_protect)
 
-class ObjectTool():
+class ObjectTool(object):
+    """
+    Base class from which all other tools should inherit. 
+    Provides setup and utility method for easily rendering a form in admin.
+    """
     def __init__(self, model):
+        """
+        Set model on which tool is acting for easy access in other methods.
+        """
         self.model = model
+    
+    def construct_form(self, request):
+        """
+        Constructs form from POST method using self.form_class.
+        """ 
+        if request.method == 'POST':
+            form = self.form_class(self.model, request.POST)
+        else:
+            form = self.form_class(self.model)
+        return form
+    
+    def media(self, form):
+        """
+        Collects admin and form media.
+        """
+        js = ['js/core.js', 'js/admin/RelatedObjectLookups.js',
+              'js/jquery.min.js', 'js/jquery.init.js']
+
+        media = forms.Media(js=['%s%s' % (settings.ADMIN_MEDIA_PREFIX, url) for url in js])
+        
+        for name, field in form.fields.iteritems():
+            media = media + field.widget.media
+
+        return media
+    
+    def reverse(self):
+        info = self.model._meta.app_label, self.model._meta.module_name, self.name
+        return reverse('object-tools:%s_%s_%s' % info)
+    
+    def _urls(self):
+        """
+        URL patterns for tool linked to _view method.
+        """
+        info = self.model._meta.app_label, self.model._meta.module_name, self.name
+        urlpatterns = patterns('',
+            url(r'^%s/$' % self.name,
+                self._view,
+                name='%s_%s_%s' % info),
+        )
+        return urlpatterns
+    urls = property(_urls)
 
     @csrf_protect_m
     def _view(self, request, extra_context=None):
@@ -37,47 +85,3 @@ class ObjectTool():
             'changelist_url': reverse('admin:%s_%s_changelist' % (app_label, object_name))
         }
         return self.view(request, extra_context)
-
-    def construct_form(self, request):
-        """
-        Constructs form from POST method using self.form_class.
-        """
-        if request.method == 'POST':
-            form = self.form_class(self.model, request.POST)
-        else:
-            form = self.form_class(self.model)
-        return form
-    
-    def media(self, form):
-        """
-        Collects admin and form media.
-        """
-
-        js = ['js/core.js', 'js/admin/RelatedObjectLookups.js',
-              'js/jquery.min.js', 'js/jquery.init.js']
-
-        media = forms.Media(js=['%s%s' % (settings.ADMIN_MEDIA_PREFIX, url) for url in js])
-        
-        for name, field in form.fields.iteritems():
-            media = media + field.widget.media
-
-        return media
-
-    def _urls(self):
-        """
-        URL patterns for tool linked to _view method.
-        """
-
-        info = self.model._meta.app_label, self.model._meta.module_name, self.name
-
-        urlpatterns = patterns('',
-            url(r'^%s/$' % self.name,
-                self._view,
-                name='%s_%s_%s' % info),
-        )
-        return urlpatterns
-    urls = property(_urls)
-    
-    def reverse(self):
-        info = self.model._meta.app_label, self.model._meta.module_name, self.name
-        return reverse('object-tools:%s_%s_%s' % info)
