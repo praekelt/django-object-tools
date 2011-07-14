efb47ce31663bccc3a331835aa4bcc10b2a3d759
Shaun Sephton <shaunsephton@gmail.com>
2011-07-14 15:23:01 +0200

    admin needed for tests

--- object_tools/tests/urls.py
+++ object_tools/tests/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import *
 
+from django.contrib import admin
+admin.autodiscover()
+
 import object_tools
 object_tools.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
     (r'^object-tools/', include(object_tools.tools.urls)),
 )
