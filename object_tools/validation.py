a989030e3de93564ea4449f3ffe4480c8e0c79cc
Shaun Sephton <shaunsephton@gmail.com>
2011-07-22 10:12:35 +0200

    dont validate from_class attribute, not strictly required

--- object_tools/validation.py
+++ object_tools/validation.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import ImproperlyConfigured
-from django.db import models
 
 __all__ = ['validate']
 
@@ -7,18 +6,11 @@ def validate(tool_class, model_class):
     """
     Does basic ObjectTool option validation. 
     """
-    # Before we can introspect models, they need to be fully loaded so that
-    # inter-relations are set up correctly. We force that here.
-    #models.get_apps()
-    
     if not hasattr(tool_class, 'name'):
         raise ImproperlyConfigured("No 'name' attribute found for tool %s." % tool_class.__name__)
     
     if not hasattr(tool_class, 'label'):
         raise ImproperlyConfigured("No 'label' attribute found for tool %s." % tool_class.__name__)
     
-    if not hasattr(tool_class, 'form_class'):
-        raise ImproperlyConfigured("No 'form_class' attribute found for tool %s." % tool_class.__name__)
-
-    if not hasattr(tool_class, 'view'):
+    if not hasattr(tool_class, 'view') and not hasattr(tool_class, '_view'):
         raise NotImplementedError("'view' method not implemented for tool %s." % tool_class.__name__)
