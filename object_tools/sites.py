35b25ec26936eda8ad6b9870e3a98eb965de6233
k8n <k2@sxw.ca>
2016-08-05 01:26:09 -0400

    Update sites.py

--- object_tools/sites.py
+++ object_tools/sites.py
@@ -64,11 +64,11 @@ class ObjectTools(object):
 
     def get_urls(self):
         try:
-            from django.conf.urls.defaults import patterns, url, include
+            from django.conf.urls.defaults import url, include
         except ImportError:
-            from django.conf.urls import patterns, url, include
+            from django.conf.urls import url, include
 
-        urlpatterns = patterns('',)
+        urlpatterns = []
 
         # Add in each object_tool's views.
         for model, object_tools in self._registry.iteritems():
@@ -80,10 +80,9 @@ class ObjectTools(object):
                 info += (model._meta.module_name,)
 
             for object_tool in object_tools:
-                urlpatterns += patterns('',
-                                        url(r'^%s/%s/' % info,
-                                            include(object_tool.urls))
-                               )
+                urlpatterns += [
+                    url(r'^%s/%s/' % info, include(object_tool.urls))
+                ]
         return urlpatterns
 
     @property
