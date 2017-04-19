22123b6e8c5dcf028426dc483e29741f1226d925
hedleyroos <hedleyroos@gmail.com>
2017-04-19 16:11:43 +0200

    Add a Django 1.10 test

--- test_settings.py
+++ test_settings.py
@@ -22,3 +22,28 @@ INSTALLED_APPS = [
 
 ROOT_URLCONF = 'object_tools.tests.urls'
 STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+        ],
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
+            ],
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ]
+        },
+    },
+]
+
