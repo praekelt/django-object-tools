b57f6c93750b0f5db1cfd30bbe8feb221b777d4b
Phala Ramonyai <phalamag@gmail.com>
2017-04-23 22:13:39 +0200

    Modify test for changelist

--- object_tools/tests/test_changelist_views.py
+++ object_tools/tests/test_changelist_views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
+from object_tools import autodiscover
+from object_tools.sites import ObjectTools
+from object_tools.tests import tools
+
 
 class ChangeListViewTestCase(TestCase):
     """
@@ -11,6 +15,7 @@ class ChangeListViewTestCase(TestCase):
         self.client.login(username="testuser", password="password")
 
     def test_tool_is_rendered(self):
+        autodiscover()
         response = self.client.get("/admin/auth/user/")
         tool_html = '<li><a href="/object-tools/auth/user/test_tool/?"' \
                     ' title=""class="historylink">Test Tool</a></li>'
