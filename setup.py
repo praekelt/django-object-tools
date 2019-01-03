ad3639293531dca140396800a1a013384c248bc5
cblignaut <cilliers@praekeltconsulting.com>
2019-01-03 13:58:18 +0200

    Update setup.py file

--- setup.py
+++ setup.py
@@ -11,16 +11,26 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     tests_require=[
-        'django-snippetscream',
+        'tox'
     ],
+    test_suite="tox",
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
-        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     zip_safe=False,
 )
